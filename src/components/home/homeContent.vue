52713ea6b0d7a7d27da100b4d7e2299df576f856
lightyisu <1513221978@qq.com>
2022-02-01 21:45:28 +0800

    update

--- src/components/home/homeContent.vue
+++ src/components/home/homeContent.vue
@@ -15,8 +15,22 @@
 
                     </el-col>
                 </el-row>
-                <i class="banner"></i>
+                <i class="banner"></i> 
+            <div class="recommend-title">
+        <span>最受关注商品</span>
         </div>
+        <el-row>
+            <el-col v-for="(item,index) in hotItems" :key="index" :span="4">
+                <div class="item-card">
+                        <i class="card-cover"></i>
+                        <p>{{item}}</p>
+                        <p :style="{fontSize:'8px',color:'gray'}">大卫作品</p>
+                        <p class="card-price">￥49</p>
+                </div>
+            </el-col>
+        </el-row>
+     </div>
+     
     </div>
 </template>
 
@@ -25,6 +39,7 @@
 <script setup>
 import {ref} from 'vue'
 let items=ref(['傲慢与偏见','张宇30讲','爱的迫降','fromis_9','乃万-风的颜色','Netflix']);
+let hotItems=ref(['傲慢与偏见','张宇30讲','爱的迫降','fromis_9','乃万-风的颜色','Netflix'])
 </script>
 
 
@@ -44,6 +59,28 @@ let items=ref(['傲慢与偏见','张宇30讲','爱的迫降','fromis_9','乃万
     .recommend-content{
        
         margin-left: 40px;
+        .item-card{
+            .card-cover{
+                height: 140px;
+                width: 14s0px;
+                display: inline-block;
+                background-color: #fff;
+
+            }
+            p{
+                margin: 4px 0;
+            }
+            .card-price{
+                margin: 20px 0;
+                color: #E82A2A;
+            }
+            height: 270px;
+            width: 150px;
+            display: inline-block;
+            background-color: #F5F5F5;
+            border-radius: 10px;
+            padding: 10px;
+        }
     }
     .banner{
         margin: 40px 0;
