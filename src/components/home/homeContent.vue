e6f09db348d69d92b5e35f5c6b7748e15948e1b2
lightyisu <1513221978@qq.com>
2022-02-03 16:41:42 +0800

    update homepage

--- src/components/home/homeContent.vue
+++ src/components/home/homeContent.vue
@@ -1,96 +1,123 @@
 <template>
-    <div class="main-content">
-        <div class="recommend-title">
-        <span>今日推荐</span>
-        <el-divider direction="vertical"></el-divider>
-        <span :style="{color:'gray'}">热门商品</span>
-        </div>
-        <div class="recommend-content">
-                <el-row >
-                    <el-col v-for="(item,index) in items" :key="index" :span="4">
-                        <i class="item-cover"></i>
-                         <p>{{item}}</p> 
-                          <el-button  size="small" round>书籍</el-button> 
-                          <el-button  size="small" round>艺术</el-button> 
 
-                    </el-col>
-                </el-row>
-                <i class="banner"></i> 
-            <div class="recommend-title">
-        <span>最受关注商品</span>
-        </div>
-        <el-row>
-            <el-col v-for="(item,index) in hotItems" :key="index" :span="4">
-                <div class="item-card">
-                        <i class="card-cover"></i>
-                        <p>{{item}}</p>
-                        <p :style="{fontSize:'8px',color:'gray'}">大卫作品</p>
-                        <p class="card-price">￥49</p>
-                </div>
-            </el-col>
-        </el-row>
-     </div>
-     
+  <div class="main-content">
+      
+    <div class="recommend-title">
+      <span>今日推荐</span>
+      <el-divider direction="vertical"></el-divider>
+      <span :style="{ color: 'gray' }">热门商品</span>
     </div>
+    <div class="recommend-content">
+      <el-row>
+        <el-col v-for="(item, index) in items" :key="index" :span="4">
+          <i class="item-cover"></i>
+          <p>{{ item }}</p>
+          <el-button size="small" round>书籍</el-button>
+          <el-button size="small" round>艺术</el-button>
+        </el-col>
+      </el-row>
+      <i class="banner"></i>
+    </div>
+
+    <div class="recommend-title">
+      <span>最受关注商品</span>
+    </div>
+    <div class="recommend-content">
+      <el-row>
+        <el-col v-for="(item, index) in hotItems" :key="index" :span="4">
+          <div class="item-card">
+            <i class="card-cover"></i>
+            <p>{{ item }}</p>
+            <p :style="{ fontSize: '8px', color: 'gray' }">大卫作品</p>
+            <p class="card-price">￥49</p>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+        <div class="recommend-footer">
+            <p>Producted By <span style="color:#25b51d">2Buy</span>.tk In 2022</p>
+            <p>Powered By Vue3 / ElementPlus</p>
+            <p>In Dev Process</p>
+            
+        </div>
+  </div>
+  
 </template>
 
-
-
 <script setup>
-import {ref} from 'vue'
-let items=ref(['傲慢与偏见','张宇30讲','爱的迫降','fromis_9','乃万-风的颜色','Netflix']);
-let hotItems=ref(['傲慢与偏见','张宇30讲','爱的迫降','fromis_9','乃万-风的颜色','Netflix'])
+import { ref } from "vue";
+let items = ref([
+  "傲慢与偏见",
+  "张宇30讲",
+  "爱的迫降",
+  "fromis_9",
+  "乃万-风的颜色",
+  "Netflix",
+]);
+let hotItems = ref([
+  "傲慢与偏见",
+  "张宇30讲",
+  "爱的迫降",
+  "fromis_9",
+  "乃万-风的颜色",
+  "Netflix",
+]);
 </script>
 
-
-
-<style scoped lang='scss'>
-.main-content{
-    .recommend-title{
-        padding: 30px;
-    }
-    .item-cover{
-        height: 200px;
-        width: 150px;
-        background-color: lightgray;
+<style scoped lang="scss">
+.main-content {
+  .recommend-title {
+    padding: 30px;
+  }
+  .item-cover {
+    height: 200px;
+    width: 150px;
+    background-color: lightgray;
+    display: inline-block;
+  }
+  .recommend-content {
+    margin-left: 40px;
+    .item-card {
+      .card-cover {
+        height: 140px;
+        width: 14s0px;
         display: inline-block;
-       
+        background-color: #fff;
+      }
+      p {
+        margin: 4px 0;
+      }
+      .card-price {
+        margin: 20px 0;
+        color: #e82a2a;
+      }
+      height: 270px;
+      width: 150px;
+      display: inline-block;
+      background-color: #f5f5f5;
+      border-radius: 10px;
+      padding: 10px;
     }
-    .recommend-content{
-       
-        margin-left: 40px;
-        .item-card{
-            .card-cover{
-                height: 140px;
-                width: 14s0px;
-                display: inline-block;
-                background-color: #fff;
-
-            }
-            p{
-                margin: 4px 0;
-            }
-            .card-price{
-                margin: 20px 0;
-                color: #E82A2A;
-            }
-            height: 270px;
-            width: 150px;
-            display: inline-block;
-            background-color: #F5F5F5;
-            border-radius: 10px;
-            padding: 10px;
-        }
-    }
-    .banner{
-        margin: 40px 0;
-        width: 1200px;
-        background: url('/banner.png');
-        display: inline-block;
-        height: 120px;
-        background-repeat: no-repeat;
-        background-size: contain;
-    }
+  }
+  .banner {
+    margin: 40px 0;
+    width: 1200px;
+    background: url("/banner.png");
+    display: inline-block;
+    height: 120px;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+  .recommend-footer{
+      font-size: 14px;
+      text-align:left;
+      padding: 30px 40px;
+      color: rgb(189, 184, 184);;
+      margin-top: 120px;
+      border-top: 1px solid rgb(223, 219, 219);
+      p{
+          margin: 0;
+      }
+  }
 }
-
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="common-layout">
-    <el-container>
+    <el-container :style="{ height: '100%' }">
       <el-aside width="200px"> <home-aside /></el-aside>
-      <el-container>
+      <el-container class="layout-content">
         <el-header>
           <home-header />
         </el-header>
+        <el-scrollbar>
         <el-main>
           <home-content />
         </el-main>
-        
+        </el-scrollbar>
       </el-container>
     </el-container>
   </div>
@@ -23,13 +24,18 @@ import HomeContent from "../components/home/homeContent.vue";
 
 <style lang="scss" scoped>
 .common-layout {
+  height: 100%;
   .el-aside {
     background: #f7f7f7;
     text-align: center;
-    height: 100vh;
+    height: 100%;
   }
   .el-header {
     border-bottom: 1.2px solid rgb(230, 230, 230);
+  }
+  .layout-content {
+    height: 100%;
+    overflow: hidden;
   }
   .el-footer {
     background: chartreuse;
